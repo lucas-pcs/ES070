e37564eb51b27bc821f18eac3689772a2fe2d2e2
Gabriel <gabrielmurizine@icloud.com>
2020-12-11 01:31:02 -0300

    criação de código main

--- Arduino/sketch_nov19a/MaquinaEstados.h
+++ Arduino/sketch_nov19a/MaquinaEstados.h
@@ -5,20 +5,20 @@
 #include "LCDInput.h"
 #include "rfidInput.h"
 
-
-
 class MaquinaEstados{
   private:
     char estado;
     char senhaAtual[4];
     Teclado  teclado;
     LCD lcd;
+    RFID rfid;
     
   public:
 
     void init(){
       teclado.init();
       lcd.init();
+      rfid.init();
     }
 
     void maquina(){
--- Arduino/sketch_nov19a/sketch_nov19a.ino
+++ Arduino/sketch_nov19a/sketch_nov19a.ino
@@ -1,11 +1,13 @@
 #include "MaquinaEstados.h"
 
+MaquinaEstados* maquina;
 
 void setup() {
   // put your setup code here, to run once:
+  maquina->init();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  maquina->maquina();
 }
